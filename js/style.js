7a0da17c27e2fca531dcf8915427009535a3439d
Simone <simoneugazio1993@gmail.com>
2019-10-23 16:17:59 +0200

    1 push

---
+++ js/style.js
@@ -0,0 +1,20 @@
+//Chiedi all’utente il cognome,
+//inseriscilo in un array con altri cognomi
+//e stampa la lista ordinata alfabeticamente.
+//Scrivi anche la posizione della lista in cui il nuovo utente si trova
+//QUINDI OUTPUT sarà:
+//- array disordinato di partenza
+//- array ordinato;
+//-  posizione in cui si trova il cognome dell’utente nell’array ordinato.
+
+
+//Chiedo cognome
+var cognome = prompt ("Scriva il suo cognome")
+
+//lista cognomi
+
+var listacognomi = ["Leone" , "Martello" , "Melchiorre" , "Monello"];
+
+//ordino la lista alfabeticamente
+
+listacognomi.sort();
